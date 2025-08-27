7fdf71dd0054c00c9f38c5e8e037310340aba2fa
anasadnan2 <anasadnanabojaber@gmail.com>
2025-08-27 14:01:51 +0300

    handle active option inside Question.create

--- app.js
+++ app.js
@@ -4,17 +4,6 @@ import { questionsData } from "./modules/questions.data.js";
 // to add active class to clicked button and remove from siblings
 const questionGroups = document.querySelectorAll(".options-answer");
 
-questionGroups.forEach((group) => {
-  const options = group.querySelectorAll(".option");
-
-  options.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      options.forEach((b) => b.classList.remove("active"));
-      btn.classList.add("active");
-    });
-  });
-});
-
 // render questions from data
 const root = document.querySelector("#quiz");
 questionsData.forEach((q) => {

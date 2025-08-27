e6478af25560dc9d6ffdc3d94854a5694e75f66e
anasadnan2 <anasadnanabojaber@gmail.com>
2025-08-27 15:06:43 +0300

    Remove unused querySelector for .options-answer

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import { Question } from "./modules/question.js";
 import { questionsData } from "./modules/questions.data.js";
 
-// to add active class to clicked button and remove from siblings
-const questionGroups = document.querySelectorAll(".options-answer");
 
 // render questions from data
 const root = document.querySelector("#quiz");

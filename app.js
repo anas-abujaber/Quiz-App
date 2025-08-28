db66be637e50ba88e300badad582929413292174
anasadnan2 <anasadnanabojaber@gmail.com>
2025-08-29 01:28:38 +0300

    catch user answer and confirm that he  chose all the questions

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 import { Question } from "./modules/question.js";
 import { questionsData } from "./modules/questions.data.js";
 
-
 // render questions from data
 const root = document.querySelector("#quiz");
 questionsData.forEach((q) => {
-  const item = new Question(q.text, q.options);
+  const item = new Question(q.id, q.text, q.options);
   root.appendChild(item.create());
 });
+
+const submitBtn = document.getElementById("submit");
+
+function getActiveButtons() {
+  return [...document.querySelectorAll(".active")];
+}
+function getAllQuestions(activeBtns) {
+  return activeBtns.map((btn) => {
+    const qId = Number(btn.closest(".question").dataset.id);
+    const siblings = Array.from(btn.parentElement.children);
+    const index = siblings.indexOf(btn);
+    return {
+      idQ: qId,
+      index,
+      answer: btn.textContent,
+    };
+  });
+}
+
+function confirmAllAnswered(activeBtns) {
+  if (activeBtns.length === questionsData.length) return true;
+  alert("You must answer all questions.");
+  return false;
+}
+
+function handleSubmit() {
+  const activeBtns = getActiveButtons();
+  if (!confirmAllAnswered(activeBtns)) return;
+  const userAnswers = getAllQuestions(activeBtns);
+}
+
+submitBtn.addEventListener("click", handleSubmit);

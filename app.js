c97810ae4bda02baa0789a2c7946bcad9359943c
anasadnan2 <anasadnanabojaber@gmail.com>
2025-09-01 13:09:01 +0300

    reformat app.js

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 import { Question } from "./modules/question.js";
 import { questionsData } from "./modules/questions.data.js";
 import { Storage } from "./modules/storage.js";
-// render questions from data
+
 const root = document.querySelector("#quiz");
+const submitBtn = document.getElementById("submit");
+const optionBtns = document.querySelectorAll(".option");
+const storage = new Storage("quiz_app");
+
+submitBtn.addEventListener("click", handleSubmit);
+
+// Render Questions
 questionsData.forEach((q) => {
   const item = new Question(q.id, q.text, q.options);
   root.appendChild(item.create());
 });
 
-const submitBtn = document.getElementById("submit");
-
+// Functions
 function getActiveButtons() {
   return [...document.querySelectorAll(".active")];
 }
+
 function getAllQuestions(activeBtns) {
   return activeBtns.map((btn) => {
     const qId = Number(btn.closest(".question").dataset.id);
     // نجيب كل الأزرار عشان نعرف ترتيب الاجابة
     const siblings = Array.from(btn.parentElement.children);
     const index = siblings.indexOf(btn);
+
     return {
       idQ: qId,
       index,
@@ -39,16 +47,17 @@ function handleSubmit() {
   const userAnswers = getAllQuestions(activeBtns);
 }
 
-submitBtn.addEventListener("click", handleSubmit);
-
-const storage = new Storage("quiz_app");
-
-const option = [...document.querySelectorAll(".option")];
-option.forEach((btn) => {
+// Storage Event
+optionBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const qId = e.currentTarget.closest(".question").dataset.id;
     const answer = e.currentTarget.textContent;
     const item = { qId, answer };
     storage.save(item);
   });
+});
+
+// Event (reload page)
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(storage.getAll()); 
 });
